3c3c5ef9d299309903ca5e6dccab43262e1d86e8
ianmmworia <ianmworia156@gmail.com>
2025-02-05 12:32:42 +0300

    implement theme toggle

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
 import { APP_NAME } from "@/lib/constants";
+import ModeToggle from "./ModeToggle";
 
 const Header = () => {
   return (
@@ -24,6 +25,7 @@ const Header = () => {
           </Link>
         </div>
         <div className="space-x-2">
+        <ModeToggle />
             <Button asChild variant='ghost'>
                 <Link href='/cart'>
                 <ShoppingCart /> cart
@@ -34,6 +36,7 @@ const Header = () => {
                 <UserIcon /> Sign In
                 </Link>
             </Button>
+            
         </div>
       </div>
     </header>
