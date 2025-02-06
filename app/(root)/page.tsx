c5501e62f79513152dffc02d03517ced59a4a0fe
ianmmworia <ianmworia156@gmail.com>
2025-02-06 09:11:07 +0300

    implement product card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,15 @@
-
+import sampledata from "@/db/sample-data";
+import ProductList from "@/components/shared/product/product-list";
 const HomePage = () => {
-  
+  return (
+    <>
+      <ProductList
+        data={sampledata.products}
+        title="Newest Arrivals"
+        limit={4}
+      />
+    </>
+  );
+};
 
-  return ( 
-    <>Prostore</>
-   );
-}
- 
 export default HomePage;
---
+++ components/shared/product/product-price.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { cn } from '@/lib/utils';
+
+const ProductPrice = ({value, className } : { value:number; className?: string; }) => {
+    // ensure 2 decimal places
+
+    const stringValue = value.toFixed(2)
+
+    const [intValue, floatValue] = stringValue.split('.')
+
+  return (
+    <p className={cn('text-2xl', className)}>
+        <span className="text-xs align-super">KES</span>
+        {intValue}
+        <span className="text-xs align-super">.{floatValue}</span>
+    </p>
+  )
+}
+
+export default ProductPrice
